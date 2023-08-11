f2ae037654ddd0e3ab8fb501e2ddca712a303c43
Laikaiyong <vandycklai@gmail.com>
2023-08-11 23:01:03 +0800

    General setup

--- pages/admin/home.h
+++ pages/admin/home.h
@@ -1,14 +1,41 @@
 #include <iostream>
+#include "../admin/displayRecord.h"
+#include "../admin/managerManagement.h"
 
 using namespace std;
 
 void adminHome(string name)
 {
-    int choice = 0;
-    cout << "\nWelcome Admin -" << name << endl;
-    cout << "\nPlease select you action\n\n1 : Add Manager\n2 : Update Manager Status\n3 : Display Tenant\n4 : Display Property\nType 0 to sign out\n\nAction: ";
-    cin >> choice;
-
-
-
+    while (true)
+    {
+        int choice = 0;
+        cout << "\nWelcome Admin -" << name << endl;
+        cout << "\nPlease select you action\n\n1 : Add Manager\n2 : Update Manager Status\n3 : Display Tenant\n4 : Display Property\nType 0 to sign out\n\nAction: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 0:
+            return;
+            break;
+        case 1:
+            addManager();
+            return;
+            break;
+        case 2:
+            modifyManagerStatus();
+            return;
+            break;
+        case 3:
+            displayTenant();
+            return;
+            break;
+        case 4:
+            displayProperty();
+            return;
+            break;
+        default:
+            cout << "\nInvalid Option, please try again.\n";
+            break;
+        }
+    }
 }
