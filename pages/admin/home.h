f11c6ce0fe7376b4c4ae47813eba0456259009f2
Berryen <fooaishen@yahoo.com>
2023-08-11 20:01:20 +0800

    update home
    easier to read :dog:  //sorryky

--- pages/admin/home.h
+++ pages/admin/home.h
@@ -1,14 +1,40 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 void adminHome(string name)
 {
-    int choice = 0;
-    cout << "\nWelcome Admin -" << name << endl;
-    cout << "\nPlease select you action\n\n1 : Add Manager\n2 : Update Manager Status\n3 : Display Tenant\n4 : Display Property\nType 0 to sign out\n\nAction: ";
-    cin >> choice;
+    int input;
+    while (true)
+    {
+        cout << "\nWelcome Admin - " << name << endl;
+        cout << "Please enter your selected option:" << endl;
+        cout << "1. Add new manager user." << endl;
+        cout << "2. Update manager status." << endl;
+        cout << "3. Display all registered tenants." << endl;
+        cout << "4. Display property information." << endl;
+        cout << "5. Sign out." << endl;
+        cin >> input;
 
-
-
+        switch (input)
+        {
+            case 1:
+                // Implement the logic for adding a new manager user.
+                break;
+            case 2:
+                // Implement the logic for updating manager status.
+                break;
+            case 3:
+                // Implement the logic for displaying all registered tenants.
+                break;
+            case 4:
+                // Implement the logic for displaying property information.
+                break;
+            case 5:
+                return;
+            default:
+                cout << "Invalid option. Please try again." << endl;
+        }
+    }
 }
